db969f021eb105e6c48ebe5522c00883b1d9642c
collins13 <rashidcollins16@gmail.com>
2021-10-19 19:30:06 +0300

    changes

--- singlelikedlist.py
+++ singlelikedlist.py
@@ -7,13 +7,37 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None;
-
+    def listLength(self):
+        currentNode = self.head;
+        length = 0;
+        while currentNode is not None:
+            currentNode = currentNode.next;
+            length += 1;
+        return length
     def insertHead(self, newNode):
         temNode = self.head
         self.head = newNode
         self.head.next = temNode;
         del temNode
+    def insertAt(self, newNode, pos):
+        if pos < 0 or pos > self.listLength():
+            print("invalide position");
+            
+        if pos == 0:
+            self.insertHead(newNode);
+            return
+        currpos = 0;
+        currNode = self.head
+        while True:
+            if currpos == pos:
+                prevNode.next = newNode;
+                newNode.next = currNode
+                break
+            prevNode = currNode
+            currNode = currNode.next;
+            currpos += 1;
 
+                
     def insert(self, newNode):
         if self.head is None:
             self.head = newNode
@@ -25,6 +49,13 @@ class LinkedList:
                     break
                 lastNode =  lastNode.next;
             lastNode.next = newNode;
+    def deleteEnd(self):
+        lastNode = self.head;
+        while lastNode.next is not None:
+            prevNode = lastNode
+            lastNode = lastNode.next;
+        # del lastNode
+        prevNode.next = None;
     def printList(self):
         currentnode = self.head
         if self.head is None:
@@ -39,11 +70,13 @@ class LinkedList:
             currentnode = currentnode.next
 
 
-node = Node("john");
+node = Node(10);
 linked = LinkedList();
 firstNode = linked.insert(node)
-node1 = Node("james");
-firstNode = linked.insert(node1)
-node2 = Node("rashid");
-firstNode = linked.insertHead(node2)
+node1 = Node(20);
+secondNode = linked.insert(node1)
+node2 = Node(15);
+thirdNode = linked.insert(node2)
+linked.deleteEnd();
 linked.printList();
+
